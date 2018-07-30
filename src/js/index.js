a8deea00bdcdc7fea73f08d101653c6ade4d8694
Harry Zachariou <hcz1@student.le.ac.uk>
2018-07-30 22:39:26 +0100

    added gifs

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,16 @@
 const fetch = require('node-fetch');
 const starwarsURL = 'https://swapi.co/api/people/';
+const gify = 'https://api.giphy.com/v1/gifs/search?api_key=nKEBs8jp1zxIOUHpWeqqll5oSvtdCOBK&limit=3&offset=0&rating=G&lang=en&q='
 
 function starwars(url,i){
     fetchData(url+i)
         .then(character => {return {name:character.name, films:character.films}})
         .then(characterObj => {
             let charName = characterObj.name;
+            getImage(charName,i);
             console.log(charName + " has been in ");
-            let createDiv =  document.createElement('div');
-            createDiv.setAttribute("id", "list"+i);
-            document.body.appendChild(createDiv)
-            let divs = document.getElementById("list"+i)
-
-            divs.innerHTML = `${charName} has been in: `;
+            
+            createDiv(i);          
             getFilms(characterObj.films, i);
             
             if(i<=10){
@@ -27,14 +25,19 @@ function starwars(url,i){
 //call function
 starwars(starwarsURL,1)
 
-function fetchData(starwarsURL){
-    return fetch(starwarsURL)
+function createDiv(i){
+    let createDiv =  document.createElement('div');
+    createDiv.setAttribute("id", "list"+i);
+    document.body.appendChild(createDiv) 
+}
+
+function fetchData(url){
+    return fetch(url)
             .then(res => res.json())
             .catch(err => console.log(err))
 }
 
 function getFilms(films, i){
-
     Promise.all(films.map(film => 
         fetchData(film)
     ))
@@ -48,4 +51,14 @@ function getFilms(films, i){
         })
 }
 
+function getImage(character, i){
+    fetchData(gify+character)
+    .then(result=> {
+
+        let divs = document.getElementById("list"+i);
+        let picURl = result.data[2].images.downsized.url;
+        return divs.innerHTML += `<img src="${picURl}"> ${character} has been in: `;
+    })
+    .catch(err=>console.log(err))
+}
 
